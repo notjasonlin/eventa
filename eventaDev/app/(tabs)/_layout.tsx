b107fc1ccf3f18a5f3dc4e4186ab6ac5d0376d09
elan-romo <elanromo@brandeis.edu>
2024-05-26 22:10:56 +0200

    Add Navbar Icons

--- eventaDev/app/(tabs)/_layout.tsx
+++ eventaDev/app/(tabs)/_layout.tsx
@@ -1,5 +1,10 @@
 import { Tabs } from "expo-router";
 import NavButton from "../../components/NavButton";
+import AntDesign from '@expo/vector-icons/AntDesign';
+import { Octicons } from '@expo/vector-icons';
+import { Ionicons } from '@expo/vector-icons';
+import { Feather } from '@expo/vector-icons';
+
 
 const TabsLayout = () => {
   return (
@@ -8,31 +13,8 @@ const TabsLayout = () => {
         name="index"
         options={{
           title: "Home",
-          headerRight: () => (<NavButton destination="(messages)/1">Messages</NavButton>)
-        }}
-      />
-      <Tabs.Screen
-        name="users/[id]"
-        options={{
-          headerTitle: "User Page",
-          title: "User",
-          headerRight: () => (<NavButton destination="(messages)/1">Messages</NavButton>)
-        }}
-      />
-      <Tabs.Screen
-        name="vendors/[vendorid]"
-        options={{
-          headerTitle: "Vendor", // Change into vendor name in the future
-          title: "Vendor",
-          headerRight: () => (<NavButton destination="(messages)/1">Messages</NavButton>)
-        }}
-      />
-      <Tabs.Screen
-        name="budget/[budgetid]"
-        options={{
-          headerTitle: "Smart Budget",
-          title: "Budget",
-          headerRight: () => (<NavButton destination="(messages)/1">Messages</NavButton>)
+          headerRight: () => (<NavButton destination="(messages)/1">Messages</NavButton>),
+          tabBarIcon: () => <AntDesign name="home" size={23} color="black" />
         }}
       />
       <Tabs.Screen
@@ -40,7 +22,35 @@ const TabsLayout = () => {
         options={{
           headerTitle: "Checklist",
           title: "Checklist",
-          headerRight: () => (<NavButton destination="(messages)/1">Messages</NavButton>)
+          headerRight: () => (<NavButton destination="(messages)/1">Messages</NavButton>),
+          tabBarIcon: () => <Octicons name="checklist" size={20} color="black" />
+        }}
+      />
+      <Tabs.Screen
+        name="vendors/[vendorid]"
+        options={{
+          headerTitle: "Vendor", // Change into vendor name in the future
+          title: "Vendor",
+          headerRight: () => (<NavButton destination="(messages)/1">Messages</NavButton>),
+          tabBarIcon: () => <Ionicons name="storefront-outline" size={24} color="black" />
+        }}
+      />
+      <Tabs.Screen
+        name="budget/[budgetid]"
+        options={{
+          headerTitle: "Smart Budget",
+          title: "Budget",
+          headerRight: () => (<NavButton destination="(messages)/1">Messages</NavButton>),
+          tabBarIcon: () => <Ionicons name="calculator-outline" size={24} color="black" />
+        }}
+      />
+      <Tabs.Screen
+        name="users/[id]"
+        options={{
+          headerTitle: "User Page",
+          title: "Profile",
+          headerRight: () => (<NavButton destination="(messages)/1">Messages</NavButton>),
+          tabBarIcon: () => <Feather name="user" size={24} color="black" />
         }}
       />
     </Tabs>

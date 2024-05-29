71a798401720bf956a3b28c4af2d75f9bb94feb0
Jason Lin <jasonlin@brandeis.edu>
2024-05-28 21:18:26 -0400

    Add functions to add new vendortype to marketplace database, and modal for users to add new vendor types of front end which will be used as example code of how to append to database.

--- eventaDev/app/(tabs)/vendors/fetchVendor.tsx
+++ eventaDev/app/(tabs)/vendors/fetchVendor.tsx
@@ -1,39 +1,60 @@
 import { supabase } from "../../../lib/supabase";
 import { useEffect, useState } from "react";
-import { Text, View } from "react-native";
+import { Pressable, Text, View } from "react-native";
 
 //component
 import VendorCard from "../../../components/vendorCard";
+import VendorModal from "../../../components/vendorModal";
 
 const FetchVendor = () => {
   const [fetchError, setFetchError] = useState(null);
   const [vendors, setVendors] = useState([]);
+  const [showModal, setShowModal] = useState(false);
+
+  const Fetch = async () => {
+    const { data: vendors, error } = await supabase
+      .from("marketplace")
+      .select("*");
+
+    if (error) {
+      setFetchError(true);
+      console.log("Error fetching vendor");
+    } else {
+      setVendors(vendors);
+      setFetchError(false);
+      console.log("Vendor fetched successfully");
+    }
+  };
+
+  const addNewItem = async (vendor) => {
+    const { data: vendors, error } = await supabase
+      .from("marketplace")
+      .insert([{ vendorType: vendor }]);
+
+    return vendors
+  };
+
+  const saveVendor = (vendor) => {
+    addNewItem(vendor)
+    .then(() => {
+      Fetch()
+    })
+  };
 
   useEffect(() => {
-    const Fetch = async () => {
-      const { data: vendors, error } = await supabase.from("marketplace").select('*');
-
-      if (error) {
-        setFetchError(true);
-        console.log("Error fetching vendor");
-      } else {
-        setVendors(vendors);
-        setFetchError(false);
-        console.log("Vendor fetched successfully");
-      }
-    };
-
     Fetch();
   }, []);
 
   return (
     <View>
-      {fetchError && (<Text>Error fetching vendor</Text>)}
-      {!fetchError && vendors.length > 0 && (
-        vendors.map((vendor) => (
-          <VendorCard key={vendor.id} vendor={vendor} />
-        ))
-      )}
+      {fetchError && <Text>Error fetching vendor</Text>}
+      {!fetchError &&
+        vendors.length > 0 &&
+        vendors.map((vendor) => <VendorCard key={vendor.id} vendor={vendor} />)}
+      <Pressable onPress={() => setShowModal(true)}>
+        <Text>Add Vendor</Text>
+      </Pressable>
+      {showModal ? <VendorModal saveNewVendor={saveVendor} hideModal={() => setShowModal(false)} /> : null}
     </View>
   );
 };

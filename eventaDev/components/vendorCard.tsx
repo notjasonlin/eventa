e7280793b71f38e5ffb35d96341e480e27308e0c
John Vnek <johnvnek@Johns-MacBook-Air.local>
2024-06-04 13:41:44 -0400

    Improved vendor marketplace and added individual vendor page

--- eventaDev/components/vendorCard.tsx
+++ eventaDev/components/vendorCard.tsx
@@ -1,12 +1,39 @@
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
+import ImageButton from "./Buttons/ImageButton";
+import { Link } from "expo-router";
 
 const VendorCard = ({ vendor }) => {
+  const type = vendor.vendorType;
+  const title = type.charAt(0).toUpperCase() + type.slice(1);
+  const DEFAULT_IMAGE = `https://meehvdwhjxszsdgpeljs.supabase.co/storage/v1/object/public/marketplace/${type}/default.png`
+
   return (
-    <View>
-      <Text>{vendor.id}</Text>
-      <Text>{vendor.vendorType}</Text>
+    <View style={styles.card}>
+      <Link href={{ pathname: "/VendorPage", params: { type: type, title: title } }}
+        asChild
+      >
+        <ImageButton uri={DEFAULT_IMAGE} />
+      </Link>
+      <Text style={styles.text}>{title}</Text>
     </View>
   );
 };
 
 export default VendorCard;
+
+const styles = StyleSheet.create({
+  card: {
+    borderColor: "#60d8ba",
+    borderRadius: 15,
+    borderWidth: 1,
+    height: 150,
+    width: 150,
+    justifyContent: "center",
+    alignItems: "center",
+    padding: 10,
+    margin: 10,
+  },
+  text: {
+    textAlign: "center",
+  }
+});

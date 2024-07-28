020865461635fc257d5cfc10af576756d1339b15
John Vnek <johnvnek@Johns-MacBook-Air-2.local>
2024-07-28 15:51:07 -0400

    Added push notifications

--- eventaDev/app/(app)/_layout.tsx
+++ eventaDev/app/(app)/_layout.tsx
@@ -2,39 +2,87 @@ import { Stack } from "expo-router";
 import NavButton from "../../components/Buttons/NavButton";
 import AntDesign from '@expo/vector-icons/AntDesign';
 import { TransitionPresets } from '@react-navigation/stack';
+import * as Device from "expo-device";
 import * as Notifications from "expo-notifications";
-import { useEffect } from "react";
+import { useEffect, useRef, useState } from "react";
 import { Alert, Platform } from "react-native";
 import { StripeProvider } from "@stripe/stripe-react-native";
+import { supabase } from "../../lib/supabase";
+import { useSelector } from "react-redux";
+import { RootState } from "../../store/redux/store";
+import Constants from "expo-constants";
 
-const RootLayout = () => {
-    useEffect(() => {
-        const configurePushNotifications = async () => {
-            const { status } = await Notifications.getPermissionsAsync();
-            let finalStatus = status;
+const configurePushNotifications = async () => {
+    const { status } = await Notifications.getPermissionsAsync();
+    let finalStatus = status;
+    let token = "";
 
-            if (finalStatus !== "granted") {
-                const { status } = await Notifications.requestPermissionsAsync();
-                finalStatus = status;
-            }
+    if (Platform.OS === "android") {
+        Notifications.setNotificationChannelAsync("default", {
+            name: "default",
+            importance: Notifications.AndroidImportance.DEFAULT,
+        });
+    }
 
-            if (finalStatus !== "granted") {
-                Alert.alert("Permissions required", "(Temporary alert, does not require permissions: add redux)");
-            }
-
-            const pushTokenData = await Notifications.getExpoPushTokenAsync();
-            console.log("Push token", pushTokenData);
-
-            if (Platform.OS === "android") {
-                Notifications.setNotificationChannelAsync("default", {
-                    name: "default",
-                    importance: Notifications.AndroidImportance.DEFAULT,
-                });
-            }
+    if (Device.isDevice) {
+        if (finalStatus !== "granted") {
+            const { status } = await Notifications.requestPermissionsAsync();
+            finalStatus = status;
         }
 
-        configurePushNotifications();
-    }, [])
+        if (finalStatus !== "granted") {
+            Alert.alert("Permissions required", "(Temporary alert, does not require permissions: add redux)");
+        }
+
+        const pushTokenData = await Notifications.getExpoPushTokenAsync({
+            projectId: Constants?.expoConfig?.extra?.eas.projectId,
+        });
+        token = pushTokenData.data;
+        // console.log("Push token", pushTokenData);
+    } else {
+        console.error("Must use physical device for push notifications")
+    }
+
+    return token;
+}
+
+const RootLayout = () => {
+    const [expoPushToken, setExpoPushToken] = useState("");
+    const [notification, setNotification] =
+        useState<Notifications.Notification>();
+    const notificationListener = useRef<Notifications.Subscription>();
+    const responseListener = useRef<Notifications.Subscription>();
+    const session = useSelector((state: RootState) => state.authentication.session);
+
+    useEffect(() => {
+        configurePushNotifications().then(async (token) => {
+            setExpoPushToken(token);
+            // console.log(token);
+
+            const { error } = await supabase
+                .from("profile")
+                .update({ expo_push_token: token })
+                .eq("id", session?.user.id);
+            console.log(error);
+        });
+
+        notificationListener.current =
+            Notifications.addNotificationReceivedListener((notification) => {
+                setNotification(notification);
+            });
+
+        responseListener.current =
+            Notifications.addNotificationResponseReceivedListener((response) => {
+                console.log(response);
+            });
+
+        return () => {
+            Notifications.removeNotificationSubscription(
+                notificationListener.current!
+            );
+            Notifications.removeNotificationSubscription(responseListener.current!);
+        };
+    }, []);
 
     return (
         <StripeProvider publishableKey={'pk_test_51Pc8OiJKz0GBX0rUq3nmv4LpaMT9DT7Rq61UKkvwHGT9x91dRyq2qZPUwlvzMMPf1yOwAxFfhkq6PkxlibirmFrq000ccVB30h' || ''} >
